29e8086d5be9edd3f50dba25b8a6126009077c4f
Elliot <35050275+apache-hb@users.noreply.github.com>
2023-10-26 19:37:57 -0400

    add optional support for std::string_view

--- common.hpp
+++ common.hpp
@@ -10,10 +10,10 @@
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -31,6 +31,11 @@
 #ifdef _MSC_VER
 #define strerror_r(e, b, l) strerror_s(b, l, e)
 #define _ARGO_WINDOWS_
+#endif
+
+#if __cpp_lib_string_view
+#include <string_view>
+#define _ARGO_STRING_VIEW_
 #endif
 
 #endif
